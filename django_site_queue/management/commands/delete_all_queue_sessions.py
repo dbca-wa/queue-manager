48c754160ce89c346d1390d07b67a10efcae41ae
Jason Moore <jason.moore@dbca.wa.gov.au>
2026-01-29 08:20:55 +0800

    Update delete queue script

--- django_site_queue/management/commands/delete_all_queue_sessions.py
+++ django_site_queue/management/commands/delete_all_queue_sessions.py
@@ -3,12 +3,19 @@ from django.utils import timezone
 # from django_site_queue import models
 import psutil
 from datetime import timedelta, datetime
+from django_site_queue import jsondb
 import requests
 
 class Command(BaseCommand):
     help = 'Delete all sessions in queue,  both active and waiting.'
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('queue_group', nargs='+', type=str)
+
     def handle(self, *args, **options):
         print ("Starting to delete Queue Sessions")
-        models.SiteQueueManager.objects.filter().delete()
+        queue_group = options['queue_group'][0]        
+        deleted_sessions = jsondb.delete_all_sessions(queue_group)
+        print ("Deleted Sessions {}".format(str(deleted_sessions)))
         print ("Finished deleting Queue Sessions")
